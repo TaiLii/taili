53924ca6078036916a16d4c49c529df8dce776ba
Tai Li <sphorq@gmail.com>
2024-11-15 01:44:03 -0500

    mobile friendliness update

--- src/pages/Gen.jsx
+++ src/pages/Gen.jsx
@@ -4,34 +4,20 @@ function Gen() {
   return (
     <div className='bg-neutral-900 flex flex-row font-rozanova'>
         <div className='flex items-center justify-center w-full'>
-            <a href="/" className='flex items-center justify-center text-xl left-0 top-0 fixed ml-20 mt-10 transition duration-150 bg-neutral-600 rounded-full p-2 shadow-lg hover:bg-neutral-700 active:bg-neutral-500'> 
+            <div className='bg-neutral-900 z-30 w-screen fixed h-40 top-0 visible lg:invisible'/>
+            <a href="/" className='z-50 flex items-center justify-center text-xl left-0 top-0 fixed ml-5 scale-75 lg:scale-100 lg:ml-20 mt-5 lg:mt-10 transition duration-150 ease-in-out bg-neutral-600 rounded-full p-2 shadow-lg hover:bg-neutral-700 active:bg-neutral-500'> 
                 <div className='*:transition *:duration-300 *:hover:invert-0 flex items-center'>
-                    <img src='./left.webp' className='w-10 invert'></img>
-                    <p className='text-gray-300 py-2 pr-5 pl-2'>Back</p>
+                    <img src='./left.webp' className='w-8 invert'></img>
+                    <p className='text-gray-300 text-md py-1 pr-2 pl-2'>Back</p>
                 </div>
             </a>
-            <div className='flex flex-col items-center justify-center pt-40'>
-                <h1 className='text-5xl text-gray-500 fixed top-14'>My Genesis Coupe</h1>   
-                <div className='-translate-x-60 border-2 border-zinc-900 bg-neutral-800 rounded-lg w-1/2 flex flex-col items-center justify-center shadow-xl'>
-                    <p className='text-neutral-100 text-3xl mt-20'>When I First got it</p>
-                    <img src="./car/1.png" alt="" className='scale-75 object-cover rounded-lg shadow-md translate-y-10'/>
-                    <div className='h-3/4'>
-                        <p className='text-white fixed right-80 translate-y-80'> &lt; Before <br/> &nbsp; After ↓</p>
-                        <img src='./car/2.png' className='scale-50 -translate-x-40 -translate-y-20 rounded-lg'/>
-                        <img src='./car/3.png' className='scale-75 translate-x-20 -translate-y-96 rounded-lg border-2'/>
-                    </div>
-                    <div className='-translate-y-96'>
-                        <div>
-                            <p className='text-white fixed right-80 translate-y-80'> &lt; Before <br/> &nbsp; After ↓</p>
-                            <img src='./car/4.png' className='scale-50 -translate-x-40 rounded-lg'/>
-                            <img src='./car/5.png' className='scale-75 translate-x-96 -translate-y-40 rounded-lg border-2 h-96 w-1/2 object-cover'/>
-                        </div>
-                        <img src='./car/6.png'/>
-                        <img src='./car/7.png'/>
-                        <img src='./car/8.png'/>
-                        <img src='./car/9.png'/>
-                    </div>
-
+            <div className='flex flex-col items-center justify-center'>
+                <h1 className='text-5xl text-gray-500 bg-neutral-900 z-50 fixed top-14 invisible lg:visible lg:flex lg:items-center lg:justify-center'>My Genesis Coupe</h1>   
+                <h1 className='text-5xl text-gray-500 fixed right-4 top-5 visible lg:invisible text-right z-50'>My Genesis <br/> Coupe</h1>   
+                <div className='border-2 border-zinc-900 bg-neutral-800 rounded-lg flex flex-col items-center justify-center shadow-xl my-40 z-0'>
+                    <p className='text-neutral-100 text-3xl my-5'>How she's sitting</p>
+                    <img src="./car/10.png" alt="" className=' object-cover rounded-lg shadow-md h-128 w-96 m-5'/>
+                    <p className='text-neutral-700'>wip...</p>
                 </div>
             </div>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,15 +18,15 @@ function Home() {
       return (
         <div className="bg-neutral-900 flex flex-row font-rozanova">
           
-          <div className="w-full">
-            <header className='fixed bg-neutral-900 w-screen pb-2'>
+          <div className="w-full z-50">
+            <header className='fixed w-screen pb-2 z-50'>
                 <nav className="flex flex-row text-xl pl-40 space-x-2 lg:invisible pt-2">
-                  <a href="#about" className={"hover:brightness-100 active:text-gray-800 transition duration-150 ease-in-out brightness-75 " + (inViewAbout ? "text-blue-500" : "text-white")}>About</a>
                   <a href="#projects" className={"hover:brightness-100 active:text-gray-800 transition duration-150 ease-in-out brightness-75 " + (inViewProject ? "text-blue-500" : "text-white")}>Projects</a>
+                  <a href="#about" className={"hover:brightness-100 active:text-gray-800 transition duration-150 ease-in-out brightness-75 " + (inViewAbout ? "text-blue-500" : "text-white")}>About</a>
                 </nav>
             </header>
             
-            <div className="top-0 left-0 p-2 fixed flex flex-row">
+            <div className="top-0 left-0 p-2 fixed flex flex-row z-10 bg-neutral-900 w-full">
                   <img src="./pika.png" alt="pika" class="h-16 w-16 rounded-md"></img>
                 
                   <div className='flex flex-col'>
@@ -42,9 +42,7 @@ function Home() {
                   </div>
             </div>
     
-            <section id="Home" ref={home} className={"transition duration-150 "+(inViewHome ? "scale-100 " : 'scale-90')}>
-                
-    
+            <section id="Home" ref={home} className={"transition duration-150 z-0"+(inViewHome ? "scale-100 " : 'scale-90')}>
                 <div className='min-h-screen flex flex-col justify-center items-center select-none'>
                   <div className="text-9xl text-fuchsia-400">
                     Tai Li
@@ -56,21 +54,20 @@ function Home() {
                 </div>
             </section>
     
-            <section id="projects" className={"h-screen flex justify-center items-center transition duration-150 "+(inViewProject ? "scale-100 " : 'scale-90')} ref={projects} >
+            <section id="projects" className={"h-screen flex justify-center items-center transition duration-150 z-10"+(inViewProject ? "scale-100 " : 'scale-90')} ref={projects} >
               <div className='flex flex-col items-center'>
                 <a href='/gen' className='scale-150 border-2 border-zinc-900 bg-neutral-800 shadow-lg rounded-lg px-16 hover:scale-[1.55] transition duration-150 ease-in-out'>
                 <div className='flex flex-row justify-center items-center text-white'>
                     <p className='font-normal'>My</p> 
                     <p className='font-sans font-bold px-1 pb-1'>'10</p>
-                    <p className='font-normal'>Gen Coupe</p> 
-                    
+                    <p className='font-normal'>Gen Coupe</p>
                 </div>
                 <img src="/car.jpg" className="flex w-52 h-52 object-cover rounded-lg m-2"/>
                 </a>
               </div>
             </section>
     
-            <section id="about" ref={about} className={"h-screen flex justify-center items-center transition duration-150 "+(inViewAbout ? "scale-100 " : 'scale-90')}>
+            <section id="about" ref={about} className={"h-screen flex justify-center items-center transition duration-150 z-0 "+(inViewAbout ? "scale-100 " : 'scale-90')}>
               <div className="text-white border-2 p-10 rounded-md text-center">
                 <p>About</p>
               </div>
@@ -80,14 +77,18 @@ function Home() {
               <img src='./616160.webp' className='w-5 invert ml-5 mb-5 hover:scale-105 transition duration-150 fixed left-0 bottom-0'></img>
             </a>
     
-            <a href="#Home" className={'flex items-center justify-center text-xl right-0 bottom-0 fixed mr-60 mb-20 transition duration-150 bg-neutral-600 rounded-full p-2 shadow-md hover:bg-neutral-700 active:bg-neutral-500 ' + (inViewHome ? "invisible" : "visible")}> 
+            <a href="#Home" className={'flex w-14 items-center justify-center left-10 bottom-0 lg:right-0 lg:bottom-0 fixed mr-60 mb-20 transition duration-150 bg-neutral-600 rounded-full p-2 shadow-md hover:bg-neutral-700 active:bg-neutral-500 visible md:visible lg:invisible xl:invisible' + (inViewHome ? "invisible" : "visible")}> 
+              <img src='./up.webp' className='w-10 invert'></img>
+            </a>
+
+            <a href="#Home" className={'flex w-14 items-center justify-center right-0 bottom-0 fixed mr-60 mb-20 transition duration-150 bg-neutral-600 rounded-full p-2 shadow-md hover:bg-neutral-700 active:bg-neutral-500 invisible lg:visible ' + (inViewHome ? "invisible" : "visible")}> 
               <img src='./up.webp' className='w-10 invert'></img>
             </a>
           </div>
           
     
           <footer className="flex items-center h-screen mr-40 fixed right-0 md:invisible invisible lg:visible">
-              <nav className="flex flex-col text-xl">
+              <nav className="flex lg:flex-col flex-row text-xl">
                 <a href="#projects" className={"hover:brightness-100 active:text-gray-800 transition duration-150 ease-in-out brightness-75 " + (inViewProject ? "text-blue-500" : "text-white")}>Projects</a>
                 <a href="#about" className={"hover:brightness-100 active:text-gray-800 transition duration-150 ease-in-out brightness-75 " + (inViewAbout ? "text-blue-500" : "text-white")}>About</a>
               </nav>
